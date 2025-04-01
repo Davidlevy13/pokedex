5012224f21315224e6125f656dcd2c779bd36c16
Davidlevy13 <levy.david1993@gmail.com>
2025-04-01 22:06:03 +0200

    restore

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard"
+import { useState } from "react";
+
+
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("bulbasaur");
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name");
+  }
+
   return (
-    <div>
-      <PokemonCard />
-    </div>
+    <>
+      <button type="button" onClick={() => setPokemonName("bulbasaure")}>
+        Bulbasaure
+      </button>
+      <button type="button" onClick={() => setPokemonName("mew")}>
+        Mew
+      </button>
+    </>
   );
 }
-
 export default App;
